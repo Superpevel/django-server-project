b795206e7b37679061f80c0da94ac40fc5e4ea1e
Superpevel <pevel@mail.ru>
2021-12-19 01:15:20 +0300

    update_ignore

--- api/settings/local_settings.py
+++ api/settings/local_settings.py
@@ -6,9 +6,9 @@ DATABASES = {
     'default':{
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'my_db',
-        'USER': 'me',
+        'USER': 'postgres',
         'PASSWORD': 'jonin123',
         'HOST': 'localhost',
-        'PORT': 5432
+        'PORT': 5433
     }
 }
