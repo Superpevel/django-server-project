857119e2604e2be3561d1120da09bf18e5a9a08f
Superpevel <pevel@mail.ru>
2022-04-07 22:04:24 +0300

    fix

--- api/libs/div_search.py
+++ api/libs/div_search.py
@@ -1,25 +1,27 @@
+ 
 def f(x):
     return pow(x,5) - pow(x,2)
+ 
 
-def div_search_new(a,b,eps):
-    n = 0 
-    step = (a-b) / 4
-    x = a
-    st_n = 1
-    while True:
+
+def div_search(a,b,eps):
+    n = 0
+    x = (a + b) / 2
+    sig = eps/2 - 0.000000000001
+    while ((b - a) >= eps):
         n+=1
-        x1 = (x+ step)
-        f1 = f(x)
-        f2 = f(x1)  
-        if f1 > f2:
-            x = x1
-            continue
-        elif abs(step) <= eps:
-            return x,n
+        x1  = (a + b) / 2 - sig
+        x2  = (a + b) / 2 + sig
+        f1 = f(x1)
+        f2 = f(x2)
+        if f1<f2:
+            b = x2
         else:
-            x = x1
-            step = -1 * step/4 
+            a = x1
+
+    return a,b,n
 
 def dv_search(a,b,eps):
-    result = div_search_new(a, b, eps)
-    return result
+    result = div_search(a, b, eps)
+    values  = list(map(lambda x: round(x,len(str(eps))- 2), result))
+    return values
--- api/libs/raz_search.py
+++ api/libs/raz_search.py
@@ -1,36 +1,27 @@
+def f(x):
+    return pow(x,5) - pow(x,2)
 
-def find_min(x):
-    value = pow(x,5) - pow(x,2)
-    return value
-
-def algo1(x0,x1,a,b,e, step,n):
-    f0 = find_min(x0)
-    f1 = find_min(x1)
-    if f0 > f1:
-        x0= x1 
-        f0 = f1
-        if  x0 > a and x0 < b:
-            return algo1(x0,x1,a,b,e,step,n+1)
+def raz_search_new(a,b,eps):
+    n = 0 
+    step = (a-b) / 4
+    x = a
+    st_n = 1
+    while True:
+        n+=1
+        x1 = (x+ step)
+        f1 = f(x)
+        f2 = f(x1)  
+        if f1 > f2:
+            x = x1
+            continue
+        elif abs(step) <= eps:
+            return x,f(x),n
         else:
-            if abs(step) < e:
-                return x0,f0,n
-            else:
-                step = -1 * (step/4)
-                return algo1(x0,x0 + step,a,b,e,step,n+1)
-    else:
-        if abs(step) < e:
-            return x0,f0,n
-        else:
-            step = -1 * (step/4)
-            return algo1(x0,x0 + step,a,b,e,step,n+1)
-            
-def algo(a,b,e):
-    step = (b-a) / 4
-    n = 0
-    return algo1(a,a+step, a, b, e, step,n)
+            x = x1
+            step = -1 * step/4 
 
 def raz_search_method(a,b,e):
-    result = list(algo(a,b,e))
+    result = list(raz_search_new(a,b,e))
     values  = list(map(lambda x: round(x,len(str(e))- 2), result))
     return values
     
