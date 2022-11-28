560123e77ae918941ae2538cb96cc5e101cbf4a3
Superpevel <pevel@mail.ru>
2022-11-29 00:35:08 +0300

    s

--- api/settings/settings.py
+++ api/settings/settings.py
@@ -43,7 +43,7 @@ CELERY_ROUTES = {
     # -- LOW PRIORITY QUEUE -- #
     # 'myapp.tasks.close_session': {'queue': 'low'},
 }
-CSRF_TRUSTED_ORIGINS = ['http://51.250.6.54','http://127.0.0.1','http://sorrytd.xyz']
+CSRF_TRUSTED_ORIGINS = ['http://51.250.6.54','http://127.0.0.1','http://sorrytd.xyz','www.sorrytd.xyz']
 # Application definition
 
 INSTALLED_APPS = [
